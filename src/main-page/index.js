7727077de9b4b7dfd93ca2469911a371950ae66f
eric kyania <erickyania0@gmail.com>
2023-08-14 20:59:17 +0300

    added the useeffect hook and downloaded the router library

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
 import "./main-page.css";
 import Header from "./header";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouses = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
   return (
     <div className="container">
       <Header subtitle="providing houses all over the world"></Header>
